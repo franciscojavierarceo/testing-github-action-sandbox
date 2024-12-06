163843a60c7f3d01b1d361b37e8139d8bbd8c466
Francisco Javier Arceo <farceo@redhat.com>
2024-12-06 14:04:53 -0500

    updated thing
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
+import re
 import shutil
+
+TAG_REGEX = re.compile(
+    r"^(?:[\/\w-]+)?(?P<version>[vV]?\d+(?:\.\d+){0,2}[^\+]*)(?:\+.*)?$"
+)
 
 def main():
     if shutil.which("git"):
         use_scm_version = {"root": ".", "relative_to": __file__, "tag_regex": TAG_REGEX}
-        print('yay it worked')
+        print('yay shutil worked')
+        print(f"use scm version = {use_scm_version}")
     else:
         use_scm_version = None
-        print('boo it no worked')
-    
+        print('boo shutil no worked')
+
 
 if __name__ == "__main__":
     main()
