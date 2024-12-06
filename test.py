d518115f5ce88928df3358e0e8a749c0ae8a9084
Francisco Javier Arceo <farceo@redhat.com>
2024-12-06 14:07:51 -0500

    printing scm version
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+from setuptools_scm import get_version
+
+# Retrieve and print the SCM version
+
+
+
 import re
 import shutil
 
@@ -10,6 +16,9 @@ def main():
         use_scm_version = {"root": ".", "relative_to": __file__, "tag_regex": TAG_REGEX}
         print('yay shutil worked')
         print(f"use scm version = {use_scm_version}")
+        scm_version = get_version(root='.', relative_to=__file__, tag_regex=TAG_REGEX)
+        print(f"SCM Version: {scm_version}")
+
     else:
         use_scm_version = None
         print('boo shutil no worked')
