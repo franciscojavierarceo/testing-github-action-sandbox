5d3995870fcc7dc0c2f858e0c30cf1fa19a543b4
Francisco Javier Arceo <farceo@redhat.com>
2024-12-06 14:02:30 -0500

    updated prints
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import shutil
 def main():
     if shutil.which("git"):
         use_scm_version = {"root": ".", "relative_to": __file__, "tag_regex": TAG_REGEX}
+        print('yay it worked')
     else:
         use_scm_version = None
+        print('boo it no worked')
     
-    print('it worked')
 
 if __name__ == "__main__":
     main()
